70cfb9cd356db226621198780adb5b94807db1fd
dragosgheorghioiu <dragosgheorghioiu9@gmail.com>
2024-06-22 12:38:31 +0300

    removed useless files

--- cmake-build-debug/libs/toml/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dregos/uni/grapevine/libs/toml
-# Build directory: /home/dregos/uni/grapevine/cmake-build-debug/libs/toml
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
